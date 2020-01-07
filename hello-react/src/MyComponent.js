593c959397231643b6d44171c4d980994f7569cb
Opdata <pb03641@gmail.com>
2020-01-08 02:47:13 +0900

    [test] props 기초 테스트

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,7 +1,12 @@
+//MyComponent.js
 import React from "react";
 
 const MyComponent = props => {
   return <div> 안녕하세요. {props.name} 입니다. </div>;
+};
+
+MyComponent.defaultProps = {
+  name: "기본 이름"
 };
 
 export default MyComponent;
