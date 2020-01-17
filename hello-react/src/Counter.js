b977acaf4f7992d7a77aed759c520133ec71f5ff
Opdata <pb03641@gmail.com>
2020-01-17 18:00:30 +0900

    [test] .setState 사용 차이 테스트

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -7,19 +7,29 @@ class Counter extends Component {
     super(props);
     //state 초기값 설정
     this.state = {
-      number: 0
+      number: 0,
+      fiexdNumber: 0
     };
   }
+
   render() {
-    const { number } = this.state; // state를 조회할때 this.state를 조회
+    const { number, fiexdNumber } = this.state; // state를 조회할때 this.state를 조회
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값 : {fiexdNumber}</h2>
         <button
-          // onClick를 통해 호출할 함수 지정
           onClick={() => {
-            //this.setState를 사용하여 값 넣는다.
-            this.setState({ number: number + 1 });
+            this.setState(prevState => {
+              return {
+                number: prevState.number + 1
+              };
+            });
+            //위와 아래는 같은 기능을 한다.
+            //아래 코드는 함수에서 바로 객체로 반환한다는 의미이다.
+            this.setState(prevState => ({
+              number: prevState.number + 1
+            }));
           }}
         >
           +1
