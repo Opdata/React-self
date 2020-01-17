58156a1435767c16f22b94b8900087f5690e54c7
Opdata <pb03641@gmail.com>
2020-01-17 11:29:53 +0900

    [test] PropTypes isRequired Test

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,39 +1,64 @@
 //MyComponent.js
-/*
-import React from "react";
 
-const MyComponent = props => {
-  const { name, children } = props;
+// import React from "react";
+
+// const MyComponent = props => {
+//   const { name, children } = props;
+//   return (
+//     <div>
+//       안녕하세요. {name} 입니다. <br />
+//       children 값은 {children} 입니다.
+//     </div>
+//   );
+// };
+
+// MyComponent.defaultProps = {
+//   name: "기본 이름"
+// };
+
+// export default MyComponent;
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// consy MyComponent = ({ name, children}) => {
+//   return (    <div>
+//     안녕하세요. {name} 입니다. <br />
+//     children 값은 {children} 입니다.
+//   </div>);
+// }
+
+// MyComponent.defaultProps = {
+//   name: '기본 이름'
+// }
+
+// MyComponent.PropTypes = {
+//   name: PropTypes.string
+// }
+
+// export default MyComponent;
+
+import React from "react";
+import PropTypes from 'prop-types'
+
+comst MyComponent = ({ name, chilrden, favoriteNumber}) => {
   return (
     <div>
-      안녕하세요. {name} 입니다. <br />
-      children 값은 {children} 입니다.
-    </div>
-  );
-};
-
-MyComponent.defaultProps = {
-  name: "기본 이름"
-};
-
-export default MyComponent;
-*/
-import React from 'react';
-import PropTypes from 'prop-types';
-
-consy MyComponent = ({ name, children}) => {
-  return (    <div>
     안녕하세요. {name} 입니다. <br />
     children 값은 {children} 입니다.
-  </div>);
-}
+    <br/>
+    제가 좋아하는 숫자는 {favoriteNumber}입니다.
+  </div>
+  );
+};
 
 MyComponent.defaultProps = {
   name: '기본 이름'
 }
 
-MyComponent.PropTypes = {
-  name: PropTypes.string
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired
 }
 
 export default MyComponent;
