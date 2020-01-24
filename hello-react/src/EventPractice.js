3b23e0e77375220dcd94c73c66cd0e85bc1fb0fc
Opdata <pb03641@gmail.com>
2020-01-24 18:32:49 +0900

    [test] 버튼 클릭시 input value 텍스트에 표시

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,27 +1,88 @@
 //EventPractive.js
 
+// import React, { Component } from "react";
+
+// class EventPractive extends Component {
+//   state = {
+//     message: "",
+//     othermessage: "",
+//     outmessage: ""
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>이벤트 발생시 텍스트 렌더링</h1>
+//         <input
+//           type="text"
+//           name="message"
+//           placeholder="아무거나 입력"
+//           onChange={e => {
+//             this.setState({ message: e.target.value });
+//             //value log
+//             //console.log(e.target.value);
+//           }}
+//         />
+//         <h1>{this.state.message}</h1>
+
+//         <h1>버튼 클릭시 텍스트 렌더링</h1>
+//         <input
+//           type="text"
+//           name="message"
+//           placeholder="작성 후 버튼 클릭"
+//           onChange={e => {
+//             this.setState({ othermessage: e.target.value });
+//             //value log
+//             //console.log(e.target.value);
+//           }}
+//         />
+//         <button
+//           onClick={() => {
+//             // alert(this.state.othermessage);
+//             this.setState({ outmessage: this.state.othermessage });
+//           }}
+//         >
+//           >확인
+//         </button>
+//         <h1>{this.state.outmessage}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+//EventPractive.js
+
 import React, { Component } from "react";
 
 class EventPractive extends Component {
   state = {
-    message: ""
+    othermessage: "",
+    outmessage: ""
   };
 
   render() {
     return (
       <div>
-        <h1>이벤트 연습</h1>
+        <h1>버튼 클릭시 텍스트 렌더링</h1>
         <input
           type="text"
           name="message"
-          placeholder="아무거나 입력"
+          placeholder="작성 후 버튼 클릭"
           onChange={e => {
-            this.setState({ message: e.target.value });
+            this.setState({ othermessage: e.target.value });
             //value log
             //console.log(e.target.value);
           }}
         />
-        <h1>{this.state.message}</h1>
+        <button
+          onClick={() => {
+            // alert(this.state.othermessage);
+            this.setState({ outmessage: this.state.othermessage });
+          }}
+        >
+          >확인
+        </button>
+        <h1>{this.state.outmessage}</h1>
       </div>
     );
   }
