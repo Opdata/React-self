059d7825510ab5d65c9d8d5f6e0c57fc6f2e84db
Opdata <pb03641@gmail.com>
2020-01-24 18:41:04 +0900

    [test] 버튼 클릭시 성공 예제 가독성 높힘

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,4 +1,4 @@
-//EventPractive.js
+//버튼 클릭시 렌더링 잘못된 예제
 
 // import React, { Component } from "react";
 
@@ -50,7 +50,45 @@
 //   }
 // }
 
-//EventPractive.js
+//버튼 클릭시 텍스트 렌더링 성공 예제
+
+// import React, { Component } from "react";
+
+// class EventPractive extends Component {
+//   state = {
+//     othermessage: "",
+//     outmessage: ""
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>버튼 클릭시 텍스트 렌더링</h1>
+//         <input
+//           type="text"
+//           name="message"
+//           placeholder="작성 후 버튼 클릭"
+//           onChange={e => {
+//             this.setState({ othermessage: e.target.value });
+//             //value log
+//             //console.log(e.target.value);
+//           }}
+//         />
+//         <button
+//           onClick={() => {
+//             // alert(this.state.othermessage);
+//             this.setState({ outmessage: this.state.othermessage });
+//           }}
+//         >
+//           >확인
+//         </button>
+//         <h1>{this.state.outmessage}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+//버튼 클릭 텍스트 렌더링 예제 가독성 높히기
 
 import React, { Component } from "react";
 
@@ -60,6 +98,23 @@ class EventPractive extends Component {
     outmessage: ""
   };
 
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      othermessage: e.target.value
+    });
+  }
+
+  onClick() {
+    this.setState({
+      outmessage: this.state.othermessage
+    });
+  }
   render() {
     return (
       <div>
@@ -68,20 +123,9 @@ class EventPractive extends Component {
           type="text"
           name="message"
           placeholder="작성 후 버튼 클릭"
-          onChange={e => {
-            this.setState({ othermessage: e.target.value });
-            //value log
-            //console.log(e.target.value);
-          }}
+          onChange={this.onChange}
         />
-        <button
-          onClick={() => {
-            // alert(this.state.othermessage);
-            this.setState({ outmessage: this.state.othermessage });
-          }}
-        >
-          >확인
-        </button>
+        <button onClick={this.onClick}>>확인</button>
         <h1>{this.state.outmessage}</h1>
       </div>
     );
