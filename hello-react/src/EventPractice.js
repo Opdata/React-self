55178069ff7597cb71c8a496858712ae9deaf632
Opdata <pb03641@gmail.com>
2020-01-28 10:45:52 +0900

    [test] onKeyPress 이벤트 테스트

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -134,6 +134,54 @@
 
 // 여러 input 다루기
 
+// import React, { Component } from "react";
+
+// class EventPractice extends Component {
+//   state = {
+//     username: "",
+//     message: ""
+//   };
+
+//   handleChange = e => {
+//     this.setState({
+//       [e.target.name]: e.target.value
+//     });
+//     console.log("name :" + e.target.name);
+//     console.log("value :" + e.target.value);
+//   };
+
+//   handleClick = () => {
+//     alert(this.state.username + ":" + this.state.message);
+//     this.setState({
+//       username: "",
+//       message: ""
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1> 이벤트 연습 </h1>
+//         <input
+//           type="text"
+//           name="username"
+//           placeholder="사용자명"
+//           value={this.state.username}
+//           onChange={this.handleChange}
+//         />
+//         <input
+//           type="text"
+//           name="message"
+//           placeholder="아무거나 입력해 보세요"
+//           value={this.state.message}
+//           onChange={this.handleChange}
+//         />
+//         <button onClick={this.handleClick}>확인</button>
+//       </div>
+//     );
+//   }
+// }
+
 import React, { Component } from "react";
 
 class EventPractice extends Component {
@@ -146,8 +194,6 @@ class EventPractice extends Component {
     this.setState({
       [e.target.name]: e.target.value
     });
-    console.log("name :" + e.target.name);
-    console.log("value :" + e.target.value);
   };
 
   handleClick = () => {
@@ -156,6 +202,10 @@ class EventPractice extends Component {
       username: "",
       message: ""
     });
+  };
+
+  handleKeyPress = e => {
+    if (e.key === "Enter") this.handleClick();
   };
 
   render() {
@@ -175,6 +225,7 @@ class EventPractice extends Component {
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
