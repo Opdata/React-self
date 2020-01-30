dd18848962b9ab37fc3ea92405e7e1f45440f024
Opdata <pb03641@gmail.com>
2020-01-30 16:32:45 +0900

    [test] 동적배열 key 응용 실습 초기 설정

--- hello-react/src/Iteration.js
+++ hello-react/src/Iteration.js
@@ -1,11 +1,19 @@
 // Iteration.js
 
-import React from "react";
+import React, { useState } from "react";
 
 const Iteration = () => {
-  const names = ["눈사람", "얼음", "눈", "눈사람"];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  return <ul>{nameList}</ul>;
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" }
+  ]);
+  const [inputText, setInpuText] = useState("");
+  const [nextId, setNextId] = useState(5); // 새로운 항목 추가시 사용할 id
+
+  const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+  return <ul>{namesList}</ul>;
 };
 
 export default Iteration;
