54adea29767a821bd2dce5a079e5654a4905dae9
Opdata <pb03641@gmail.com>
2020-02-03 12:03:04 +0900

    [test] LifeCycle 테스트

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -37,11 +37,37 @@
 //   }
 // }
 
-import React from "react";
-import Iteration from "./Iteration";
+// import React from "react";
+// import Iteration from "./Iteration";
 
-const App = () => {
-  return <Iteration />;
-};
+// const App = () => {
+//   return <Iteration />;
+// };
 
+import React, { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+class App extends Component {
+  state = {
+    color: "#000000"
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}> 랜덤 색상 </button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
+  }
+}
 export default App;
